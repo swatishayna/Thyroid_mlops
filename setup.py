37ebbe84c822338b10f3bc488073d75d38f164e7
Swati <swati308mmu@gmail.com>
2021-10-30 13:06:30 +0530

    tox added setup.py build

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a thyroid prediction package",
+    author="swati",
+    packages=find_packages(),
+    license="MIT"
+)
---
+++ tests/test_config.py
@@ -0,0 +1,8 @@
+
+
+## Always define a function with test_
+
+def test_generic():
+    a =2
+    b =2
+    assert a==b
