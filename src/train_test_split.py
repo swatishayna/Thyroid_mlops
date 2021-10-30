4c8cc7090f443520eff0c0d77d046b6980c246c0
Swati <swati308mmu@gmail.com>
2021-10-30 12:09:21 +0530

    stage 4 model training added

--- src/train_test_split.py
+++ src/train_test_split.py
@@ -7,7 +7,7 @@ import argparse
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-
+from utils.training import scaled_df,under_sampled
 
 
 
@@ -25,6 +25,14 @@ def split_traintest_save(config_path):
     train_filepath  = os.path.join(dirpath,train_filename)
     test_filename   = config['traintestsplit']['test_filename']
     test_filepath   = os.path.join(dirpath,test_filename)
+    
+    all_col = config['traintestsplit']['all_data']
+    scaler_model_dir = config['traintestsplit']['scaler_model_dir']
+    scaler_filename = config['traintestsplit']['scaler_filename']
+    scaler_filepath = os.path.join(scaler_model_dir,    scaler_filename)
+    target_col = config['base']['target_col']
+
+
 
     random_state = config['base']['random_state']
     split_ratio = config['traintestsplit']['test_size']
@@ -33,11 +41,20 @@ def split_traintest_save(config_path):
     #reading the dataset
     df = pd.read_csv(processed_data_filepath)
 
+    #scaling the dataset
+    df_scaled = scaled_df(df[all_col],scaler_filepath,target_col)
+    #data is highly imbalanced we will perform undersampling on train_data
+    train_downsampled = under_sampled(df_scaled[all_col],target_col)
+
+
+
     #splitting the data set
-    train, test = train_test_split(df,
+    train, test = train_test_split(train_downsampled,
                 test_size=split_ratio,
                 random_state = random_state
     )
+
+
 
 
     train.to_csv(train_filepath, sep = ',', index = False, encoding = 'utf-8')
---
+++ src/utils/training.py
@@ -0,0 +1,42 @@
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.utils import resample
+import gzip, pickle, pickletools
+import pandas as pd
+
+# def get_correlation(df):
+#     corr_values = abs(df.corr(method='spearman')['Class'])
+#     corr_values = corr_values.drop('Class')
+#     corr_values = corr_values[corr_values > 0.03]
+#     return corr_values
+
+
+
+
+def scaled_df(df, scaler_fileppath,label):
+    scaler = MinMaxScaler()
+    
+    data=pd.DataFrame(scaler.fit_transform(df.drop(columns =['Class'])),columns=df.columns[:-1])
+    pickle.dump(scaler, open(scaler_fileppath,'wb'))
+    data[label] = df[label]
+    return data
+
+# def smote_resampled(X_train, X_test, y_train, y_test):
+#     smote = SMOTE('not majority',random_state = 1)
+#     X_train_sm, y_train_sm = smote.fit_resample(X_train,y_train)
+#     X_test_sm, y_test_sm = smote.fit_resample(X_test,y_test)
+#     return X_train_sm,X_test_sm,y_train_sm,y_test_sm
+
+
+def under_sampled(df,target_col):
+    df_negative = df[df[target_col]==0]
+    df_hypothyroid = df[df[target_col]==1]
+    df_hyperthyroid = df[df[target_col]==2]
+    df_sickeuthyroid = df[df[target_col]==3]
+
+    df_negative_downsampled = resample(df_negative,replace=False,n_samples=300,random_state=1)
+    df_hypothyroid_downsampled = resample(df_hypothyroid,replace=False,n_samples=300,random_state=1)
+    df_hyperthyroid_downsampled = resample(df_hyperthyroid,replace=False,n_samples=300,random_state=1)
+    
+    df_downsampled = pd.concat([df_negative_downsampled,df_hypothyroid_downsampled,df_hyperthyroid,df_sickeuthyroid])
+    return df_downsampled
